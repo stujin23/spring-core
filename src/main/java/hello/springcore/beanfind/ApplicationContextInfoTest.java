894428a4919ffc5a3ff3bea441a2df070a4feeb9
user <stujin23@gmail.com>
2024-03-27 13:54:02 +0900

    first commit 생성자 주입 좋은객체지향.md

--- src/main/java/hello/springcore/beanfind/ApplicationContextInfoTest.java
+++ src/main/java/hello/springcore/beanfind/ApplicationContextInfoTest.java
@@ -1,35 +1,37 @@
 package hello.springcore.beanfind;
 
 import hello.springcore.AppConfig;
-import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class ApplicationContextInfoText {
+public class ApplicationContextInfoTest {
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
     @Test
     @DisplayName("모든 빈 출력하기")
-    public void findAllBean(){
+    public void findAllBean() {
         String[] beanDefinitionNames = ac.getBeanDefinitionNames();
         for (String beanDefinitionName : beanDefinitionNames) {
             Object bean = ac.getBean(beanDefinitionName);
-            System.out.println("name="+beanDefinitionName +"object=" +bean);
+            System.out.println("name=" + beanDefinitionName + ", object=" + bean);
         }
     }
+
     @Test
     @DisplayName("애플리케이션 빈 출력하기")
-    void findApplicationBean(){
+    void findApplicationBean() {
         String[] beanDefinitionNames = ac.getBeanDefinitionNames();
-        for(String beanDefinitionName : beanDefinitionNames) {
+        for (String beanDefinitionName : beanDefinitionNames) {
             BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
 
-            //Role ROLE_APPLICATION (애플리케이션에 등록한 빈)
-            //Role ROLE_INFRASTRUCTURE (스프링 내부에서 사용하는 빈)
-            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION){
+//            Role ROLE_APPLICATION (애플리케이션에 등록된 빈)
+//            Role ROLE_INFRASTRUCTURE (스프링 내부에서 사용하는 빈)
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
                 Object bean = ac.getBean(beanDefinitionName);
+                System.out.println("name=" + beanDefinitionName + ", Object=" + bean);
             }
-            System.out.println("name=" + beanDefinition +", Object=" + bean);
         }
     }
 }
