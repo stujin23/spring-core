f03c716c6b9d359416372819de40c32e53ea3fa6
user <stujin23@gmail.com>
2024-03-27 11:43:28 +0900

    spring

--- src/main/java/hello/springcore/MemberApp.java
+++ src/main/java/hello/springcore/MemberApp.java
@@ -4,11 +4,16 @@ import hello.springcore.member.Grade;
 import hello.springcore.member.Member;
 import hello.springcore.member.MemberService;
 import hello.springcore.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "meberA", Grade.VIP);
         memberService.join(member);
 
